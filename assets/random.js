da2f4064290538cb78e80b89421be3903ea61f96
yand3r3d3v <53695101+yand3r3d3v@users.noreply.github.com>
2022-03-19 11:20:33 +0300

    Add files via upload

---
+++ assets/random.js
@@ -0,0 +1,13 @@
+var assilka = document.getElementById("a1");
+var min = Math.ceil(1);
+var max = Math.floor(3);
+var randomInt = Math.floor(Math.random() * (max - min + 1) + min);
+if (randomInt == 1) {
+   assilka.href = "https://vk.com/yand3r3d3v";
+}
+if (randomInt == 2) {
+   assilka.href = "tg://resolve?domain=yand3r3d3v";
+}  
+if (randomInt == 3) {
+   assilka.href = "https://github.com/yand3r3d3v";
+}    
